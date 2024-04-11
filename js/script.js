7ae0d91abea106dcd54b6319560b6ee50b082778
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-04-11 15:20:03 +0200

    feat: project scaffolding

---
+++ js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// Definisco un array di oggetti
+
+const teamMember = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        pic: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        name: "Angela Caroll"
+    }
+]
